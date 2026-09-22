ac65cf8e1e9e85ab844e0bd0c28665c3bf945d08
JB3-Gamer <j3lampabernardo@gmail.com>
2026-09-22 16:26:41 +0800

    Answers for Quiz 1

---
+++ Quiz1/src/Main.java
@@ -0,0 +1,71 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int[] numbers = new int[10];
+
+        numbers[0] = 35;
+        numbers[1] = 69;
+        numbers[2] = 1;
+        numbers[3] = 10;
+        numbers[4] = -50;
+        numbers[5] = 320;
+        numbers[6] = 63;
+        numbers[7] = 58;
+        numbers[8] = 26;
+        numbers[9] = 13;
+
+        bubbleSort(numbers);
+        System.out.println("During bubble sort:");
+        printArrayElements(numbers);
+
+        selectionSort(numbers);
+        System.out.println("\n\nDuring selection sort:");
+        printArrayElements(numbers);
+
+    }
+
+    private static void bubbleSort(int[] arr)
+    {
+        for (int lastSortedIndex = arr.length - 1; lastSortedIndex > 0; lastSortedIndex--)
+        {
+            for (int i = 0; i < lastSortedIndex; i++)
+            {
+                if (arr[i] < arr[i + 1])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr [i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+    }
+
+    private static void selectionSort(int[] arr)
+    {
+        for (int lastSortedIndex = arr.length - 1; lastSortedIndex > 0; lastSortedIndex--)
+        {
+            int smallestIndex = 0;
+
+            for (int i = 1; i <= lastSortedIndex; i++)
+            {
+                if (arr[i] < arr[smallestIndex])
+                {
+                    smallestIndex = i;
+                }
+            }
+
+            int temp = arr[lastSortedIndex];
+            arr[lastSortedIndex] = arr[smallestIndex];
+            arr[smallestIndex] = temp;
+        }
+    }
+
+    private static void printArrayElements(int[] arr)
+    {
+        for (int j : arr)
+        {
+            System.out.print(j + " ");
+        }
+    }
+}
